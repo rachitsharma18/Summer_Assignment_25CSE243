5f4490bd66c83e67e17e0c29066ec05392ca5c81
RACHIT SHARMA <rachiiiitttt18@gmail.com>
2026-06-05 21:30:43 +0530

    Program-5.java

--- DAY-2/Program-5.java
+++ DAY-2/Program-5.java
@@ -1,1 +1,26 @@
-.
+import java.util.Scanner;
+
+public class Program5 {
+    public static void main(String[] args) {
+
+        // Input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number: ");
+        int n = sc.nextInt();
+
+        // Sum
+        int sum = 0;
+
+        while (n != 0) {
+            int digit = n % 10;
+            sum += digit;
+            n /= 10;
+        }
+
+        // Output
+        System.out.println("Sum of Digits = " + sum);
+
+        sc.close();
+    }
+}
