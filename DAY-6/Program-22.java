8101b681af7da0a417db3903858fadf04f9d783c
Rachit Sharma <rachiiiitttt18@gmail.com>
2026-06-08 20:59:00 +0530

    Day-6 Uploaded

--- DAY-6/Program-22.java
+++ DAY-6/Program-22.java
@@ -1,21 +1,32 @@
-#include <stdio.h>
-#include <math.h>
+import java.util.Scanner;
 
-int main() {
-    long long binary;
-    int decimal = 0, i = 0, remainder;
+public class Program26 {
+    public static void main(String[] args) {
 
-    printf("Enter a binary number: ");
-    scanf("%lld", &binary);
+        // Input
+        Scanner sc = new Scanner(System.in);
 
-    while (binary != 0) {
-        remainder = binary % 10;
-        decimal += remainder * pow(2, i);
-        binary /= 10;
-        i++;
+        System.out.print("Enter Binary Number: ");
+        int binary = sc.nextInt();
+
+        // Variables
+        int decimal = 0;
+        int power = 0;
+
+        // Conversion
+        while (binary != 0) {
+
+            int digit = binary % 10;
+
+            decimal += digit * (int)Math.pow(2, power);
+
+            power++;
+            binary /= 10;
+        }
+
+        // Output
+        System.out.println("Decimal = " + decimal);
+
+        sc.close();
     }
-
-    printf("Decimal Number = %d\n", decimal);
-
-    return 0;
 }
