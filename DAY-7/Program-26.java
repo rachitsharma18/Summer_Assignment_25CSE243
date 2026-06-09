3acda929df1844024e4dd11905ed25854a606643
Rachit Sharma <rachiiiitttt18@gmail.com>
2026-06-09 12:53:11 +0530

    Day-7 Uploaded

---
+++ DAY-7/Program-26.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class Program22 {
+
+    // Fibonacci
+    static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        if (n == 1) {
+            return 1;
+        }
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String[] args) {
+
+        // Input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number of Terms: ");
+        int n = sc.nextInt();
+
+        // Output
+        System.out.println("Fibonacci Series:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+
+        sc.close();
+    }
+}
