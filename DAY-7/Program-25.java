6cb7a54e33c62f68d383317c46a964ee70bcb306
Rachit Sharma <rachiiiitttt18@gmail.com>
2026-06-09 12:52:41 +0530

    Day-7 Uploaded

---
+++ DAY-7/Program-25.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Program21 {
+
+    // Factorial
+    static int factorial(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        }
+
+        return n * factorial(n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        // Input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number: ");
+        int n = sc.nextInt();
+
+        // Output
+        System.out.println("Factorial = " + factorial(n));
+
+        sc.close();
+    }
+}
