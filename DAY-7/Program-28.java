35ac3beaf6a7850577ddb0b17030c0c5523c2820
Rachit Sharma <rachiiiitttt18@gmail.com>
2026-06-09 12:54:01 +0530

    Day-7 Uploaded

---
+++ DAY-7/Program-28.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Program24 {
+
+    // Reverse
+    static int reverse = 0;
+
+    static int reverseNumber(int n) {
+        if (n == 0) {
+            return reverse;
+        }
+
+        reverse = reverse * 10 + (n % 10);
+
+        return reverseNumber(n / 10);
+    }
+
+    public static void main(String[] args) {
+
+        // Input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number: ");
+        int n = sc.nextInt();
+
+        // Output
+        System.out.println("Reversed Number = " + reverseNumber(n));
+
+        sc.close();
+    }
+}
