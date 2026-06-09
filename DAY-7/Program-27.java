000b2264e9cfa072adbd8a33a1f0471e72339a17
Rachit Sharma <rachiiiitttt18@gmail.com>
2026-06-09 12:53:38 +0530

    Day-7 Uploaded

---
+++ DAY-7/Program-27.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Program23 {
+
+    // Sum of Digits
+    static int sumDigits(int n) {
+        if (n == 0) {
+            return 0;
+        }
+
+        return (n % 10) + sumDigits(n / 10);
+    }
+
+    public static void main(String[] args) {
+
+        // Input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number: ");
+        int n = sc.nextInt();
+
+        // Output
+        System.out.println("Sum of Digits = " + sumDigits(n));
+
+        sc.close();
+    }
+}
