99bf9b3e7a4031d49f8cc30851d81390007ecced
Rachit Sharma <rachiiiitttt18@gmail.com>
2026-06-10 21:03:59 +0530

    Day-8 Uploaded
    Implement a program to print a number pattern based on user input.

---
+++ DAY-8/Program-30.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Program30 {
+    public static void main(String[] args) {
+
+        // Input
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number of Rows: ");
+        int rows = sc.nextInt();
+
+        // Pattern
+        for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
