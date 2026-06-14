0a6197babca9171876a99fb185ac9322c96b5799
Rachit Sharma <rachiiiitttt18@gmail.com>
2026-06-14 18:12:34 +0530

    Day-47.java

---
+++ Day-12/Program-47.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Program47 {
+
+    // Function
+    static void fibonacci(int n) {
+
+        int a = 0;
+        int b = 1;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.print(a + " ");
+
+            int c = a + b;
+            a = b;
+            b = c;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Number of Terms: ");
+        int n = sc.nextInt();
+
+        fibonacci(n);
+
+        sc.close();
+    }
+}
