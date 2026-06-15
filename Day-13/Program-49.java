d63f6c732f0223abeaa3b0d0543571c2847bb20b
Rachit Sharma <rachiiiitttt18@gmail.com>
2026-06-15 20:12:56 +0530

    Day-13 Uploaded

---
+++ Day-13/Program-49.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Program49 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Size of Array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter Elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.println("Array Elements:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
