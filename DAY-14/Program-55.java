cade7742d106f07a7a3ce65048e3e3141cd236c1
Rachit Sharma <rachiiiitttt18@gmail.com>
2026-06-17 08:14:13 +0530

    Day-14 Uploaded

---
+++ DAY-14/Program-55.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class Program55 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Size of Array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter Elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < n; i++) {
+
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            } else if (arr[i] > secondLargest && arr[i] != largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println("Second Largest = " + secondLargest);
+
+        sc.close();
+    }
+}
