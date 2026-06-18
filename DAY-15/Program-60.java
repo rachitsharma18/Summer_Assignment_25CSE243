be03a8b1901712433ff70c6593be9c2633cae746
Rachit Sharma <rachiiiitttt18@gmail.com>
2026-06-18 10:03:06 +0530

    Day-15 Uploaded

---
+++ DAY-15/Program-60.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Program60 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Size of Array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter Elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        int index = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (arr[i] != 0) {
+                arr[index] = arr[i];
+                index++;
+            }
+        }
+
+        while (index < n) {
+            arr[index] = 0;
+            index++;
+        }
+
+        System.out.println("Array After Moving Zeroes:");
+
+        for (int i = 0; i < n; i++) {
+            System.out.print(arr[i] + " ");
+        }
+
+        sc.close();
+    }
+}
