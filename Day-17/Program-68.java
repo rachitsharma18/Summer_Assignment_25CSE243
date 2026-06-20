9e63ff2965ba026b8aea63743ba669a9f5a3be4b
Rachit Sharma <rachiiiitttt18@gmail.com>
2026-06-20 21:51:23 +0530

    Day-17 Uploaded

---
+++ Day-17/Program-68.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Program68 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Size of First Array: ");
+        int n1 = sc.nextInt();
+
+        int arr1[] = new int[n1];
+
+        for (int i = 0; i < n1; i++) {
+            arr1[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter Size of Second Array: ");
+        int n2 = sc.nextInt();
+
+        int arr2[] = new int[n2];
+
+        for (int i = 0; i < n2; i++) {
+            arr2[i] = sc.nextInt();
+        }
+
+        System.out.println("Common Elements:");
+
+        for (int i = 0; i < n1; i++) {
+
+            for (int j = 0; j < n2; j++) {
+
+                if (arr1[i] == arr2[j]) {
+                    System.out.print(arr1[i] + " ");
+                    break;
+                }
+            }
+        }
+
+        sc.close();
+    }
+}
