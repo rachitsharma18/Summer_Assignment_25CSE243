991fcee8870057cd71e2399912a4800538ca6560
Rachit Sharma <rachiiiitttt18@gmail.com>
2026-06-20 13:36:23 +0530

    Day-16 Uploaded

---
+++ DAY-16/Program-63.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Program63 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Size of Array: ");
+        int n = sc.nextInt();
+
+        int arr[] = new int[n];
+
+        System.out.println("Enter Elements:");
+
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        System.out.print("Enter Target Sum: ");
+        int sum = sc.nextInt();
+
+        boolean found = false;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = i + 1; j < n; j++) {
+
+                if (arr[i] + arr[j] == sum) {
+                    System.out.println("Pair: " + arr[i] + " " + arr[j]);
+                    found = true;
+                }
+            }
+        }
+
+        if (!found) {
+            System.out.println("No Pair Found");
+        }
+
+        sc.close();
+    }
+}
