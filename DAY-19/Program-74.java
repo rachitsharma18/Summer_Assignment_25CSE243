36b0b5084371d87f6bae9ebe16570fe0d072127f
Rachit Sharma <rachiiiitttt18@gmail.com>
2026-06-23 14:36:21 +0530

    Day-19 Uploaded

---
+++ DAY-19/Program-74.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class Program74 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a[][] = new int[2][2];
+        int b[][] = new int[2][2];
+        int c[][] = new int[2][2];
+
+        System.out.println("Enter First Matrix:");
+
+        for(int i = 0; i < 2; i++) {
+            for(int j = 0; j < 2; j++) {
+                a[i][j] = sc.nextInt();
+            }
+        }
+
+        System.out.println("Enter Second Matrix:");
+
+        for(int i = 0; i < 2; i++) {
+            for(int j = 0; j < 2; j++) {
+                b[i][j] = sc.nextInt();
+            }
+        }
+
+        for(int i = 0; i < 2; i++) {
+            for(int j = 0; j < 2; j++) {
+                c[i][j] = a[i][j] - b[i][j];
+            }
+        }
+
+        System.out.println("Result:");
+
+        for(int i = 0; i < 2; i++) {
+            for(int j = 0; j < 2; j++) {
+                System.out.print(c[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
