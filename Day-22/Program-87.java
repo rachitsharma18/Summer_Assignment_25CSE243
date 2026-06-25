d64f162e55c0be7868668f139ece77d77726d110
Rachit Sharma <rachiiiitttt18@gmail.com>
2026-06-25 23:49:24 +0530

    Day-22 Uploaded

---
+++ Day-22/Program-87.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Program87 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter String: ");
+        String str = sc.nextLine();
+
+        System.out.print("Enter Character: ");
+        char ch = sc.next().charAt(0);
+
+        int count = 0;
+
+        for(int i = 0; i < str.length(); i++) {
+            if(str.charAt(i) == ch) {
+                count++;
+            }
+        }
+
+        System.out.println("Frequency = " + count);
+
+        sc.close();
+    }
+}
