2805b33030a993af51570ba58e87930bcfb6e4e8
Rachit Sharma <rachiiiitttt18@gmail.com>
2026-06-25 23:17:07 +0530

    Day-21 Uploaded

---
+++ DAY-21/Program-84.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Program84 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter String: ");
+        String str = sc.nextLine();
+
+        System.out.println(str.toUpperCase());
+
+        sc.close();
+    }
+}
