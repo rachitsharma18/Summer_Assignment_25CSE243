c55d33b8d65568479c527f4e731622535784cdff
Rachit Sharma <rachiiiitttt18@gmail.com>
2026-06-25 23:50:24 +0530

    Day-22 Uploaded

---
+++ Day-22/Program-88.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Program88 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter String: ");
+        String str = sc.nextLine();
+
+        str = str.replace(" ", "");
+
+        System.out.println(str);
+
+        sc.close();
+    }
+}
