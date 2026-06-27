34ca4e53ecc8f0dd8c060af7c2512a2b7d5ff055
Rachit Sharma <rachiiiitttt18@gmail.com>
2026-06-27 09:41:45 +0530

    Day-23 Uploaded

---
+++ DAY-23/Program-89.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class Program89 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter String: ");
+        String str = sc.nextLine();
+
+        for(int i = 0; i < str.length(); i++) {
+
+            int count = 0;
+
+            for(int j = 0; j < str.length(); j++) {
+                if(str.charAt(i) == str.charAt(j)) {
+                    count++;
+                }
+            }
+
+            if(count == 1) {
+                System.out.println(str.charAt(i));
+                break;
+            }
+        }
+
+        sc.close();
+    }
+}
